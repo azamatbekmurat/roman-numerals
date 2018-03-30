24336e6256582644382afddc7bbe5f1e36ed94e5
Azamat Bekmuratov and Eric Wolfe <azamat+eric@>
2018-03-30 16:09:45 -0700

    add script2 file for refactoring initial js code

--- js/scripts2.js
+++ js/scripts2.js
@@ -1,15 +1,21 @@
 var outputRoman=[];
 
-var romanNumerals = ["I","V","X","L","C","D","M"];
-var decimals = [1,5,10,50,100,500,1000];
+var romanNumerals = ["M","CM","D","CD","C","XC","L","XL,""X","IX","V","IV","I"];
+
+var decimals = [1000,900,500,400,100,90,50,40,10,9,5,4,1];
 
 function romans(number) {
-  if (number>0&&number<4) {
-    for (var i=0; i < number; i++) {
-      outputRoman+= romanNumerals[0];
-    }
+  for (var i=0; i<=number.length; i++) {
+    if (number>decimals)
   }
+
 }
+  // if (number>0&&number<4) {
+  //   for (var i=0; i < number; i++) {
+  //     outputRoman+= romanNumerals[0];
+  //   }
+  // }
+
 
 
 $(document).ready(function() {
